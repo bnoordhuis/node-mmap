e10fb5dac073c10d01a6f2db16eae7c3c3b26f67
Ben Noordhuis <info@bnoordhuis.nl>
2011-01-15 01:19:59 +0100

    Export a single method map() that returns a native node.js Buffer instance that is backed by the mmap()'ed file.

--- test.js
+++ test.js
@@ -2,8 +2,8 @@ fs = require('fs');
 sys = require('sys');
 mmap = require('mmap');
 assert = require('assert');
+constants = require('constants');
 
-Buffer = mmap.Buffer;
 PAGESIZE = mmap.PAGESIZE;
 PROT_READ = mmap.PROT_READ;
 MAP_SHARED = mmap.MAP_SHARED;
@@ -15,32 +15,32 @@ fd = fs.openSync(process.argv[1], 'r');
 size = fs.fstatSync(fd).size;
 
 // full 5-arg constructor
-buffer = new Buffer(size, PROT_READ, MAP_SHARED, fd, 0);
+buffer = mmap.map(size, PROT_READ, MAP_SHARED, fd, 0);
 assert.equal(buffer.length, size);
 
 // short-hand 4-arg constructor
-buffer = new Buffer(size, PROT_READ, MAP_SHARED, fd);
+buffer = mmap.map(size, PROT_READ, MAP_SHARED, fd);
 assert.equal(buffer.length, size);
 
 // page size is almost certainly >= 4K and this script isn't that large...
 fd = fs.openSync(process.argv[1], 'r');
-buffer = new Buffer(size, PROT_READ, MAP_SHARED, fd, PAGESIZE);
+buffer = mmap.map(size, PROT_READ, MAP_SHARED, fd, PAGESIZE);
 assert.equal(buffer.length, size);	// ...but this is according to spec
 
 // zero size should throw exception 
 fd = fs.openSync(process.argv[1], 'r');
 try {
-  buffer = new Buffer(0, PROT_READ, MAP_SHARED, fd, 0);
+	buffer = mmap.map(0, PROT_READ, MAP_SHARED, fd, 0);
 } catch (e) {
-  assert.equal(e.errno, process.EINVAL);
+	assert.equal(e.errno, constants.EINVAL);
 }
 
 // non-page size offset should throw exception
 if (PAGESIZE != 1) {
-  fd = fs.openSync(process.argv[1], 'r');
-  try {
-    buffer = new Buffer(size, PROT_READ, MAP_SHARED, fd, 1);
-  } catch (e) {
-    assert.equal(e.errno, process.EINVAL);
-  }
+	fd = fs.openSync(process.argv[1], 'r');
+	try {
+		buffer = mmap.map(size, PROT_READ, MAP_SHARED, fd, 1);
+	} catch (e) {
+		assert.equal(e.errno, constants.EINVAL);
+	}
 }

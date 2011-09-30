f1e1916656bf6f30110075a15b754a54ecd8ead3
alexstup <alexstup@gmail.com>
2011-10-01 00:16:29 +0300

    msync function

--- mmap.cc
+++ mmap.cc
@@ -12,6 +12,20 @@ namespace {
 using namespace v8;
 using namespace node;
 
+Handle<Value> MSync(const Arguments& args) {
+	if (args.Length() < 2) {
+		return ThrowException(Exception::Error(String::New("Msync takes two argument: Buffer, flag(MS_ASYNC or MS_SYNC).")));
+	}
+	Buffer* buffer = ObjectWrap::Unwrap<Buffer>(args[0]->ToObject());
+	const int flag = args[1]->ToInteger()->Value();
+	int err = msync((void *)Buffer::Data(buffer), Buffer::Length(buffer), flag);
+	if (err == -1) {
+		return ThrowException(ErrnoException(errno, "msync", ""));
+	} else {
+		return True();
+	}
+}
+
 void Unmap(char* data, void* hint) {
 	munmap(data, (size_t) hint);
 }
@@ -58,8 +72,11 @@ extern "C" void init(Handle<Object> target) {
 	target->Set(String::New("MAP_PRIVATE"), Integer::New(MAP_PRIVATE), attribs);
 	target->Set(String::New("PAGESIZE"), Integer::New(sysconf(_SC_PAGESIZE)),
 			attribs);
+	target->Set(String::New("MS_ASYNC"), Integer::New(MS_ASYNC), attribs);
+	target->Set(String::New("MS_SYNC"), Integer::New(MS_SYNC), attribs);
 
 	target->Set(String::NewSymbol("map"), FunctionTemplate::New(Map)->GetFunction());
+	target->Set(String::NewSymbol("msync"), FunctionTemplate::New(MSync)->GetFunction());
 }
 
 }
